e5b9816eb4afba01bf7f736d0d558d71de43004e
selestab <niaestella@gmail.com>
2022-09-27 05:41:53 -0500

    fix: se anadio ataque aleatorio enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,9 +1,16 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMascotaJugsador = document.getElementById("boton-mascota")
-    botonMascotaJugsador.addEventListener("click" , seleccionarMascotaJugador )
+    botonMascotaJugsador.addEventListener("click", seleccionarMascotaJugador)
 
-window.addEventListener("load", iniciarJuego)    
-
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-Agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 
 
@@ -13,43 +20,74 @@ function seleccionarMascotaJugador() {
     let inputCapipepo = document.getElementById("Capipepo")
     let inputRatigueya = document.getElementById("Ratigueya")
     let spanMascotaJugador = document.getElementById("mascota-jugador")
-    
+
 
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = "Hipodogue"
     }
-        
+
     else if (inputCapipepo.checked) {
         spanMascotaJugador.innerHTML = "Capipepo"
     }
-        
+
     else if (inputRatigueya.checked) {
         spanMascotaJugador.innerHTML = "Ratigueya"
     }
+
     else {
         alert("Selecciona una mascota")
     }
-    SeccionarMascotaEnemigo()
 
-          
+    SeccionarMascotaEnemigo()
 }
-function SeccionarMascotaEnemigo(){
-    let ataqueAleatorio = aleatorio (1,3)
+
+function SeccionarMascotaEnemigo() {
+    let mascotaAleatorio = aleatorio(1, 3)
     let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
-    if (ataqueAleatorio == 1){
-        spanMascotaEnemigo. innerHTML = "Hipodogue"
+    if (mascotaAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodogue"
     }
-    else if (ataqueAleatorio == 2){
-        spanMascotaEnemigo. innerHTML = "Capipepo"
+    else if (mascotaAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "Capipepo"
     }
     else {
-        spanMascotaEnemigo. innerHTML = "Ratigueya"
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
     }
-  
+
 }
+
+function ataqueFuego() {
+    ataqueJugador = "Fuego"
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+    ataqueJugador = "Agua"
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra() {
+    ataqueJugador = "Tierra"
+    ataqueAleatorioEnemigo()
+}
+function ataqueAleatorioEnemigo() {
+    ataqueAleatorio = aleatorio(1, 3)
+
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = "fuego"
+    }
+    else if (ataqueEnemigo == 2) {
+        ataqueEnemigo = "Agua"
+    }
+    else {
+        ataqueEnemigo = "Tierra"
+    }
+}
+
+
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener("load", iniciarJuego)
+    // otra manera de llamar al scropt despues de que se carguen todo el html.
+    // la funcion inicisr fuego se carga cuando todo el contenido este cargado.
